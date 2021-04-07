058878f4bdd7ede877f5fd4f48c1c34d492a6767
NotMichaelsGitHub <notdipaolo@outlook.com>
2021-04-06 21:02:45 -0500

    sad face test

--- components/Arewe.js
+++ components/Arewe.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 const Arewe = () => {
   return(
     <Container className="text-center p-5">
-      <h1>Not Yet</h1>
+      <h1>Not Yet :(</h1>
     </Container>
   )
 };
